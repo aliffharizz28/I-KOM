f6d31d82f07ed4d3360866dbb41d28705788ee57
Aliff <a203272@siswa.ukm.my>
2026-05-17 17:53:47 +0800

    feat: implement modern split-screen login page with animated branding and responsive design

--- ikom/resources/views/login.blade.php
+++ ikom/resources/views/login.blade.php
@@ -51,10 +51,7 @@
                     </div>
                 </div>
             </div>
-            <!-- Subtle geometric shapes for visual depth -->
-            <div class="shape shape-1"></div>
-            <div class="shape shape-2"></div>
-            <div class="shape shape-3"></div>
+
         </div>
 
         <!-- Right Side: Login Form -->
