f568240f7c21f03830d0005e9ce777408331ec1f
Aliff Hariz <a203272@siswa.ukm.edu.my>
2026-06-18 12:48:22 +0800

    cubaan 2

--- ikom/resources/views/bottombar.blade.php
+++ ikom/resources/views/bottombar.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="{{ asset('css/stylebottombar.css') }}">
 <footer class="bottom-bar">
     <div class="bottom-bar-content">
-        <p>Copyright &copy; 2026 I-KOM | Developed by Ahmad Aliff Hariz bin Azmi</p>
+        <p>Copyright &copy; 2026 I-KOM | Developed by Ahmad Aliff Hariz bin Azmi cubaan nak test working pipeline</p>
     </div>
 </footer>
